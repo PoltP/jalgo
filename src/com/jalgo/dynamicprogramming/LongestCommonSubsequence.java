6bf48f957026e10563feb27a3564599fe6780605
Pavel Poltavets <pavel.a.poltavets@gmail.com>
2019-10-06 00:06:17 +0300

    Dynamic Programming - Longest Common Subsequence (tabulation approach)

--- src/com/jalgo/dynamicprogramming/LongestCommonSubsequence.java
+++ src/com/jalgo/dynamicprogramming/LongestCommonSubsequence.java
@@ -1,6 +1,22 @@
 package com.jalgo.dynamicprogramming;
 
 public class LongestCommonSubsequence {
+    public static int dpTabulation(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] memo = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    memo[i][j] = 1 + memo[i - 1][j - 1];
+                } else {
+                    memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
+                }
+            }
+        }
+        return memo[n][m];
+    }
+
     public static int dpMemoization(String s1, String s2) {
         int[][] memo = new int[s1.length() + 1][s2.length() + 1];
         return dpMemoizationCore(memo, s1, s2, s1.length(), s2.length());
--- src/com/jalgo/dynamicprogramming/test/LongestCommonSubsequenceTest.java
+++ src/com/jalgo/dynamicprogramming/test/LongestCommonSubsequenceTest.java
@@ -6,9 +6,18 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class LongestCommonSubsequenceTest {
+    static String[] test1 = new String[]{"ABCDEFGHIJKLMNOPQRSTUVWXYZ", "PAVEL"};
+    static String[] test2 = new String[]{"The only thing we have to fear is fear itself", "thing is fear"};
+
     @Test
     public void dpMemoization() {
-        assertEquals(3, LongestCommonSubsequence.dpMemoization("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "PAVEL"));
-        assertEquals(13, LongestCommonSubsequence.dpMemoization("The only thing we have to fear is fear itself", "thing is fear"));
+        assertEquals(3, LongestCommonSubsequence.dpMemoization(test1[0], test1[1]));
+        assertEquals(13, LongestCommonSubsequence.dpMemoization(test2[0], test2[1]));
+    }
+
+    @Test
+    public void dpTabulation() {
+        assertEquals(3, LongestCommonSubsequence.dpTabulation(test1[0], test1[1]));
+        assertEquals(13, LongestCommonSubsequence.dpTabulation(test2[0], test2[1]));
     }
 }
