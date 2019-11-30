2a1d9e7e5780cc4ddbbb7413164f9258e9cb1e3e
Pavel Poltavets <pavel.a.poltavets@gmail.com>
2019-11-30 23:57:44 +0300

    QuickSelect (find Kth smallest element) - fix tests

--- src/com/jalgo/other/test/QuickSelectTest.java
+++ src/com/jalgo/other/test/QuickSelectTest.java
@@ -3,21 +3,25 @@ package com.jalgo.other.test;
 import com.jalgo.other.QuickSelect;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static junit.framework.TestCase.assertEquals;
 
 
 public class QuickSelectTest {
-    final Integer[] testArray = new Integer[]{30, -70, 90, 10, -5, 80, 60, -100, 60};
+    static Integer[] getTestArray() {
+        return new Integer[]{30, -70, 90, 10, -5, 80, 60, -100, 60};
+    }
 
     @Test
     public void lomuto() {
-        assertEquals(80, (int) QuickSelect.findKthSmallestLomuto(testArray, (a, b) -> Integer.compare(a, b), 2, 6, 3));
-        assertEquals(-5, (int) QuickSelect.findKthSmallestLomuto(testArray, (a, b) -> Integer.compare(a, b), 0, testArray.length - 1, 2));
+        assertEquals(80, (int) QuickSelect.findKthSmallestLomuto(getTestArray(), (a, b) -> Integer.compare(a, b), 2, 6, 3));
+        assertEquals(-5, (int) QuickSelect.findKthSmallestLomuto(getTestArray(), (a, b) -> Integer.compare(a, b), 0, getTestArray().length - 1, 2));
     }
 
     @Test
     public void hoare() {
-        assertEquals(80, (int) QuickSelect.findKthSmallestHoare(testArray, (a, b) -> Integer.compare(a, b), 2, 6, 3));
-        assertEquals(-5, (int) QuickSelect.findKthSmallestLomuto(testArray, (a, b) -> Integer.compare(a, b), 0, testArray.length - 1, 2));
+        assertEquals(80, (int) QuickSelect.findKthSmallestHoare(getTestArray(), (a, b) -> Integer.compare(a, b), 2, 6, 3));
+        assertEquals(-5, (int) QuickSelect.findKthSmallestHoare(getTestArray(), (a, b) -> Integer.compare(a, b), 0, getTestArray().length - 1, 2));
     }
 }
